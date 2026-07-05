2047c03661a3577f5f1d335c26e472288095b21b
i16nagi <i16.chebbah@gmail.com>
2026-07-05 18:08:39 +0200

    first commit + setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/login/login.component';
+import { SignUpComponent } from './features/sign-up/sign-up.component';
+import { NotFoundComponent } from './features/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'auth',
+        children: [
+            {
+                path: 'login',
+                component: LoginComponent
+            },
+            {
+                path: 'sign-up',
+                component: SignUpComponent
+            }
+        ]
+    },
+    { path: '**',
+        component: NotFoundComponent
+    }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('cookMasterApp');
+  protected readonly title = signal('Cook Master ');
 }
